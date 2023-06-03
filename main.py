4a797b592045ece3e95215b659e5113b7f5b620e
viditya <viditya.tyagi@gmail.com>
2023-06-03 14:41:37 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from text_Summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from text_Summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from text_Summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 from text_Summarizer.logging import logger
+from text_Summarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from text_Summarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -20,6 +24,39 @@ try:
     logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<')
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX============X')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data Transformation stage'
+
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX============X')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Model Trainer stage'
+
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX============X')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Model Evaluation stage'
+
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX============X')
 except Exception as e:
     logger.exception(e)
